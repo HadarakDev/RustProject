240761480e72024ee68cd0d0ad826b5ffad4e732
nicolasR <nico_roche@hotmail.fr>
2020-01-24 14:56:50 +0100

    create struct & impl fn

--- ppm/src/lib.rs
+++ ppm/src/lib.rs
@@ -1,5 +1,28 @@
-strut Pixel{
-    r: i8,
-    g: i8,
-    b: i8,
+#[derive(Clone, Copy)]
+struct Pixel{
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Pixel {
+    fn new(r: u8, g: u8, b: u8) -> Pixel {
+        Pixel {
+            r: r,
+            g: g,
+            b: b,
+        }
+    }
+
+    fn red(&self) -> u8 {
+        self.r
+    }
+
+    fn green(&self) -> u8 {
+        self.g
+    }
+
+    fn blue(&self) -> u8 {
+        self.b
+    }
 }

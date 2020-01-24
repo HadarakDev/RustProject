51dca0cc7e01ceea4d049460ab8fc62b451fe567
nicolasR <nico_roche@hotmail.fr>
2020-01-24 15:35:40 +0100

    add test + display + invert

--- ppm/src/lib.rs
+++ ppm/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Copy)]
 struct Pixel{
     r: u8,
@@ -25,4 +27,44 @@ impl Pixel {
     fn blue(&self) -> u8 {
         self.b
     }
+
+    fn invert(&mut self)
+    {
+        self.r = 0xFF - self.r;
+        self.g = 0xFF - self.g;
+        self.b = 0xFF - self.b;
+    }
+}
+
+impl fmt::Display for Pixel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       write!(f, "r = {}, g = {}, b = {}", self.r, self.g, self.b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Pixel;
+
+    #[test]
+    fn test_create_pixel() {
+        let p = Pixel{r:0, g:0, b:0};
+
+        assert_eq!(p.red(), 0);
+        assert_eq!(p.green(), 0);
+        assert_eq!(p.blue(), 0);
+    }
+
+    #[test]
+    fn test_invert_pixel() {
+        let mut p = Pixel{r:0, g:0, b:0};
+
+        p.invert();
+        assert_eq!(p.red(), 255);
+        assert_eq!(p.green(), 255);
+        assert_eq!(p.blue(), 255);
+    
+    }
+
+
 }

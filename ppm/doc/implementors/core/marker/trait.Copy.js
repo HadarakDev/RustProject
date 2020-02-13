1e0122f45592aee31fa84a27c21304f68479dcb3
nicolasR <nico_roche@hotmail.fr>
2020-02-13 16:39:54 +0100

    update doc and add display image with colorized squared

--- ppm/doc/implementors/core/marker/trait.Copy.js
+++ ppm/doc/implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["doc"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"doc/struct.Pixel.html\" title=\"struct doc::Pixel\">Pixel</a>",synthetic:false,types:["doc::Pixel"]},];
+implementors["docs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"docs/struct.Pixel.html\" title=\"struct docs::Pixel\">Pixel</a>",synthetic:false,types:["docs::Pixel"]},];
 implementors["lib"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"lib/struct.Pixel.html\" title=\"struct lib::Pixel\">Pixel</a>",synthetic:false,types:["lib::Pixel"]},];
 
             if (window.register_implementors) {
--- ppm/doc/source-files.js
+++ ppm/doc/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["doc"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["docs"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["lib"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
